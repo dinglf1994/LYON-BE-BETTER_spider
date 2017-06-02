f801f941f4e9a81ca9b8f78b79118fb4a31d51ae
dinglf1994 <13120310025@163.com>
2017-06-03 00:14:44 +0800

    folder

---
+++ bat/startT.bat
@@ -0,0 +1,8 @@
+@echo off
+cd ../demo
+start php search.tech-food.com.food.time.php > ../data/runtime/search.tech-food.com.food.time.php.log
+start php search.tech-food.com.meat.time.php > ../data/runtime/search.tech-food.com.meat.time.php.log
+start php search.tech-food.com.milk.time.php > ../data/runtime/search.tech-food.com.milk.time.php.log
+start php search.tech-food.com.wine.time.php > ../data/runtime/search.tech-food.com.wine.time.php.log
+start php www.xinhuanet.com.php > ../data/runtime/www.xinhuanet.com.time.php.log
+exit
